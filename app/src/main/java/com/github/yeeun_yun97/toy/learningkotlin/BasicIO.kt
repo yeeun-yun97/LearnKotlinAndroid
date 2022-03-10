22803112fb64da7019375256689dc03fb5ca2111
yyn9704@gmail.com <yyn9704@gmail.com>
2022-03-10 23:35:46 +0900

    0310 basic input output

---
+++ app/src/main/java/com/github/yeeun_yun97/toy/learningkotlin/BasicIO.kt
@@ -0,0 +1,16 @@
+package com.github.yeeun_yun97.toy.learningkotlin
+
+fun main( args: Array<String>){
+    println("Hello")
+    println("안녕하세요!")
+    println(100)
+
+    print("안녕")
+    print("하세요")
+    print("!")
+    println()
+
+    print("> ")
+    var line = readLine()
+    println(line)
+}
